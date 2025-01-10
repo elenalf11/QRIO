0dd000de7fa4eba74bc2affc2b3b35e3623de482
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2025-01-10 19:06:37 +0100

    Header comenzado

--- proyecto/tailwind.config.mjs
+++ proyecto/tailwind.config.mjs
@@ -3,6 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			color1: '#EAD8C5',
+			color2: 'F4E8DF',
+			color3: 'F4F0ED',
+			color4: 'EB585B',
+		},
+		fontFamily: {
+			'fuente': ['League Spartan'],	
+		},
+
 	},
 	plugins: [],
 }
