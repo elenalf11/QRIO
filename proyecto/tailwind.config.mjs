f8ad848646f4e980689c119c779eb7044f3b313d
cris <cristina.cg9@hotmail.com>
2025-01-17 18:37:03 +0100

    Finished animation

--- proyecto/tailwind.config.mjs
+++ proyecto/tailwind.config.mjs
@@ -2,16 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'fuente': ['League Spartan', 'serif'],	
+			},
+		},
 		colors: {
 			color1: '#EAD8C5',
 			color2: 'F4E8DF',
 			color3: 'F4F0ED',
 			color4: 'EB585B',
 		},
-		fontFamily: {
-			'fuente': ['League Spartan'],	
-		},
+		
 
 	},
 	plugins: [],
