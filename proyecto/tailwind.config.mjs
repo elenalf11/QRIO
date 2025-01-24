879113ff9ebc2478d1176d533cc4831e852c0416
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2025-01-24 18:07:53 +0100

    Header animado y linkeado con todas las paginas del proyecto

--- proyecto/tailwind.config.mjs
+++ proyecto/tailwind.config.mjs
@@ -9,9 +9,9 @@ export default {
 		},
 		colors: {
 			color1: '#EAD8C5',
-			color2: 'F4E8DF',
-			color3: 'F4F0ED',
-			color4: 'EB585B',
+			color2: '#F4E8DF',
+			color3: '#F4F0ED',
+			color4: '#EB585B',
 		},
 		
 
